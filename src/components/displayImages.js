bdb43c147669163c7e920d155c80412453b4ebb0
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-01 18:18:03 +0100

    Images resizing

--- src/components/displayImages.js
+++ src/components/displayImages.js
@@ -1,51 +1,66 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 
 export default function QuiltedImageList() {
 	return (
-		<Container style={{ py: 4 }}>
-			<Box display='flex'>
-				<>
-					<img
-						src={
-							'https://images.unsplash.com/photo-1546015720-b8b30df5aa27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-						}
-						alt='Bébé'
-						style={{ width: 560, height: 400, pr: 3 }}
-					/>
-				</>
+		<Container display='flex' style={{ py: 4 }}>
+			<div
+				sx={{
+					width: '90%',
+					height: 100,
+				}}>
+				<img
+					src={
+						'https://images.unsplash.com/photo-1546015720-b8b30df5aa27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+					}
+					alt='Bébé'
+					style={{ width: 450, height: 300, pr: 3 }}
+				/>
+			</div>
 
-				<div style={{ p: 2 }}>
-					<img
-						src={
-							'https://images.unsplash.com/photo-1542037104857-ffbb0b9155fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=754&q=80'
-						}
-						alt='famille'
-						style={{ width: 560, height: 400, mr: 2 }}
-					/>
-				</div>
-			</Box>
-			<Box display='flex'>
-				<div style={{ py: 2 }}>
-					<img
-						src={
-							'https://images.unsplash.com/photo-1501341810975-6a8bfd95c6ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=828&q=80'
-						}
-						alt='Mariage'
-						style={{ width: 560, height: 400, m: 3 }}
-					/>
-				</div>
-				<div style={{ py: 2 }}>
-					<img
-						src={
-							'https://images.unsplash.com/photo-1566516171511-1c411a59c8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-						}
-						alt='Batême'
-						style={{ width: 560, height: 400, m: 3 }}
-					/>
-				</div>
-			</Box>
+			<div
+				style={{ p: 2 }}
+				sx={{
+					width: '90%',
+					height: 100,
+				}}>
+				<img
+					src={
+						'https://images.unsplash.com/photo-1542037104857-ffbb0b9155fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=754&q=80'
+					}
+					alt='famille'
+					style={{ width: 450, height: 300, mr: 2 }}
+				/>
+			</div>
+
+			<div
+				style={{ py: 2 }}
+				sx={{
+					width: '90%',
+					height: 100,
+				}}>
+				<img
+					src={
+						'https://images.unsplash.com/photo-1501341810975-6a8bfd95c6ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=828&q=80'
+					}
+					alt='Mariage'
+					style={{ width: 450, height: 300, m: 3 }}
+				/>
+			</div>
+			<div
+				style={{ py: 2 }}
+				sx={{
+					width: '90%',
+					height: 100,
+				}}>
+				<img
+					src={
+						'https://images.unsplash.com/photo-1566516171511-1c411a59c8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+					}
+					alt='Batême'
+					style={{ width: 450, height: 300, m: 3 }}
+				/>
+			</div>
 		</Container>
 	);
 }
