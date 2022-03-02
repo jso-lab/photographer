5cd23b6ab97e2018a9f96a4e61acfbce37fcbb54
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-02 11:19:45 +0100

    remove style on div

--- src/components/displayImages.js
+++ src/components/displayImages.js
@@ -4,11 +4,7 @@ import Container from '@mui/material/Container';
 export default function QuiltedImageList() {
 	return (
 		<Container display='flex' style={{ py: 4 }}>
-			<div
-				sx={{
-					width: '100%',
-					height: 100,
-				}}>
+			<div>
 				<img
 					src={
 						'https://images.unsplash.com/photo-1546015720-b8b30df5aa27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
@@ -18,12 +14,7 @@ export default function QuiltedImageList() {
 				/>
 			</div>
 
-			<div
-				style={{ p: 2 }}
-				sx={{
-					width: '90%',
-					height: 100,
-				}}>
+			<div style={{ p: 2 }}>
 				<img
 					src={
 						'https://images.unsplash.com/photo-1542037104857-ffbb0b9155fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=754&q=80'
@@ -33,12 +24,7 @@ export default function QuiltedImageList() {
 				/>
 			</div>
 
-			<div
-				style={{ py: 2 }}
-				sx={{
-					width: '90%',
-					height: 100,
-				}}>
+			<div style={{ py: 2 }}>
 				<img
 					src={
 						'https://images.unsplash.com/photo-1501341810975-6a8bfd95c6ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=828&q=80'
@@ -47,12 +33,7 @@ export default function QuiltedImageList() {
 					style={{ width: 450, height: 300, m: 3 }}
 				/>
 			</div>
-			<div
-				style={{ py: 2 }}
-				sx={{
-					width: '90%',
-					height: 100,
-				}}>
+			<div style={{ py: 2 }}>
 				<img
 					src={
 						'https://images.unsplash.com/photo-1566516171511-1c411a59c8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
